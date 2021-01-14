67ae9c482b3accd93209842cd4b6d6a19788253d
withewolf-dev <itskashyap26@gmail.com>
2021-01-14 23:19:53 +0530

    minor app.js fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import React from "react"
 
 
 function MyApp({ Component, pageProps }) {
