f3e482d182ad1bef240b20d5ee13d98809194724
withewolf-dev <itskashyap26@gmail.com>
2021-01-14 23:39:19 +0530

    added _document.js

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/styles";
+import React from "react";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
